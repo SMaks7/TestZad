9c2fc03a99d847e6a8c970f130dc6663f499998f
Smaks <smaks.biz@gmail.com>
2020-12-20 22:18:46 +0500

    Надеюсь получиться 2

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ var stop bool
 
 func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "Сказочные Олени, Всем привет!")
+	fmt.Fprintf(w, "Сказочные Олени, Вам большой привет!")
 	stop = true
 }
 func main(){
